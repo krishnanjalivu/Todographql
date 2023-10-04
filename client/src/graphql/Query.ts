f3ff58778137f150c16a796d0b37f77af596f555
krishnanjalivu <vukrishnanjali@gmail.com>
2023-10-04 16:13:15 +0530

    first commit

---
+++ client/src/graphql/Query.ts
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+export const GET_TODOS = gql`
+  {
+    todos{
+      id
+      description
+      done
+      
+    }
+  }
+  
+`;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
+const client = new ApolloClient({
+  uri: 'https://api.mocki.io/playground?endpoint=https://api.mocki.io/v2/c4d7a195/graphql',
+  cache: new InMemoryCache(),
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+  <App />
+</ApolloProvider>
 );
